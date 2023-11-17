08f94d3ea816aa2fd3e52bef0ab9639e8b04962b
Vadym Sirenko <vadym.syrenko@brokenbuild.net>
2023-11-17 02:41:56 +0200

    feature: layout min height

--- src/components/common/PublicLayout/index.tsx
+++ src/components/common/PublicLayout/index.tsx
@@ -1,13 +1,17 @@
 import React, { type FC } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import s from './index.module.scss'
+
 import { Header } from '../Header'
 import { Footer } from '../Footer'
 
 export const PublicLayout: FC = () => (
-    <div>
-        <Header/>
-        <Outlet/>
+    <div className={s.wrapper}>
+        <div>
+            <Header/>
+            <Outlet/>
+        </div>
         <Footer/>
     </div>
 )
