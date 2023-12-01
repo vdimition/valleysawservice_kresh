b8e32eb3c8cdc13721c7c944041f677a4868e50c
Vadym Sirenko <vadym.syrenko@brokenbuild.net>
2023-12-01 14:36:10 +0200

    test: render App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import { App } from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Saw Sharpening Service in the Greater Phoenix, AZ Area/i)
   expect(linkElement).toBeInTheDocument()
 })
